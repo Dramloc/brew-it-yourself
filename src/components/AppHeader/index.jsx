4406841a369e978ccf78eb08799dc91d059ed8c5
Antoine Colmard <a.colmard@gmail.com>
2018-07-09 22:53:43 +0200

    :lipstick: Rework app header design

--- src/components/AppHeader/index.jsx
+++ src/components/AppHeader/index.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'preact-router/match';
+import logo from '../../assets/images/logo.png';
 import manifest from '../../manifest.json';
 import style from './style';
 
 function AppHeaderLink(props) {
 	return (
 		<Link
-			className={style.AppNavigation_Link}
-			activeClassName={style.AppNavigation_Link__active}
+			className={style.AppHeader_Link}
+			activeClassName={style.AppHeader_Link__active}
 			{...props}
 		/>
 	);
@@ -14,13 +15,16 @@ function AppHeaderLink(props) {
 
 export function AppHeader() {
 	return (
-		<header className={style.AppNavigation}>
-			<h1>{manifest.name}</h1>
-			<nav>
-				<AppHeaderLink href="/">
-					<span>Recipes</span>
-				</AppHeaderLink>
-			</nav>
+		<header className={style.AppHeader}>
+			<div>
+				<img src={logo} alt="The Brew It Yourself logo: hand drawn hops" />
+				<h1>{manifest.name}</h1>
+				<nav>
+					<AppHeaderLink href="/">
+						<span>Recipes</span>
+					</AppHeaderLink>
+				</nav>
+			</div>
 		</header>
 	);
 }
