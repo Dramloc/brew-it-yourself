f97fc456695410ecb8544c26b74967cfdc234c39
Antoine Colmard <a.colmard@gmail.com>
2018-07-11 00:49:46 +0200

    :hammer: Extract metadata fetch generator to a function

--- src/state/recipes/doFetchRecipes.js
+++ src/state/recipes/doFetchRecipes.js
@@ -1,16 +1,9 @@
-import fetch from 'isomorphic-fetch';
+import { getMetadataFetch } from '../metadata/getMetadataFetch';
 import { RECIPES_FAILURE, RECIPES_REQUEST, RECIPES_SUCCESS } from './types';
 
 const API_URL = 'https://api.punkapi.com/v2/beers';
+const doFetch = getMetadataFetch({
+	types: [RECIPES_REQUEST, RECIPES_SUCCESS, RECIPES_FAILURE]
+});
 
-export const doFetchRecipes = () => async dispatch => {
-	dispatch({ type: RECIPES_REQUEST });
-	try {
-		const response = await fetch(API_URL);
-		const recipes = await response.json();
-		dispatch({ type: RECIPES_SUCCESS, payload: recipes });
-	}
-	catch (error) {
-		dispatch({ type: RECIPES_FAILURE, payload: error });
-	}
-};
+export const doFetchRecipes = () => doFetch(API_URL);
