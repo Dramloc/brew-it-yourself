fad2949e136bafc885d201445c5c8cb3a0690e70
Antoine Colmard <a.colmard@gmail.com>
2018-07-14 22:59:49 +0200

    :bug: Replace <a> with router <Link>

--- src/components/RecipeList/Item/index.jsx
+++ src/components/RecipeList/Item/index.jsx
@@ -1,4 +1,5 @@
 import cx from 'classnames';
+import { Link } from 'preact-router';
 import style from './style.scss';
 
 function Image({ src, alt, ...props }) {
@@ -16,10 +17,10 @@ function Text(props) {
 export function RecipeListItem({ recipe, className, ...props }) {
 	return (
 		<li className={cx(style.RecipeListItem, className)} {...props}>
-			<a href={`/recipes/${recipe.id}`}>
+			<Link href={`/recipes/${recipe.id}`}>
 				<Image src={`https://placeimg.com/720/405/beer/${recipe.id % 10}`} alt={recipe.name} />
 				<Text title={recipe.name}>{recipe.name}</Text>
-			</a>
+			</Link>
 		</li>
 	);
 }
